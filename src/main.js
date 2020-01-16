f858074596c4041984014a80b840a281e7ede9cc
wang-ming-biao <1340921277>
2020-01-16 17:41:22 +0800

    完成登录页面静态布局

--- src/main.js
+++ src/main.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import store from './store'
+// 导入base
+import './style/base.css'
 // 导入vant插件
 import Vant from 'vant'
 import 'vant/lib/index.css'
--- src/views/login/login.vue
+++ src/views/login/login.vue
@@ -1,21 +1,25 @@
 <template>
   <div class="bodyBox">
-    <van-nav-bar class="topBar" title="登陆" />
+    <van-nav-bar class="topBar" title="登录" />
     <van-cell-group>
-      <van-field v-model="obj.mobile" left-icon="smile-o" right-icon="warning-o" placeholder="请输入手机号" >
+      <van-field v-model="obj.mobile"  placeholder="请输入手机号" >
           <template slot="left-icon">
               <span class="iconfont icon-phone my-icon"></span>
           </template>
       </van-field>
-      <van-field v-model="obj.code" clearable left-icon="music-o" placeholder="请输入验证码" >
+      <van-field v-model="obj.code" clearable  placeholder="请输入验证码" >
                     <template slot="left-icon">
               <span class="iconfont icon-suo my-icon"></span>
           </template>
           <template slot="button">
-              <van-button round size="small" color="#ededed" class="codeBtn">发送验证码</van-button>
+              <van-button round size="small" color="#ededed" class="codeBtn">获取验证码</van-button>
           </template>
       </van-field>
     </van-cell-group>
+    <van-button size="large" class="loginBtn">登录</van-button>
+    <div class="clause">
+      <a href="#">隐私条款</a>
+    </div>
   </div>
 </template>
 
@@ -28,11 +32,19 @@ export default {
         code: ''// 验证码
       }
     }
+  },
+  methods: {
+
   }
 }
 </script>
 
 <style lang="less" scoped>
+.bodyBox{
+  position: relative;
+  height: 100%;
+  width: 100%;
+}
 // 导航栏背景色及文字颜色
 .bodyBox .topBar {
   background-color: #3296fa;
@@ -47,5 +59,18 @@ export default {
   span{
     color: black;
   }
+}
+.loginBtn{
+  background:rgba(109,180,251,1);
+border-radius:10px;
+color: #fff;
+margin: 33px 20px;
+width: 88%;
+}
+.clause{
+position: absolute;
+bottom: 40px;
+width: 100%;
+text-align: center;
 }
 </style>
