627e4571e3a010baf3bcab4f670f5720d4edd6db
wang-ming-biao <1340921277>
2020-02-01 20:24:27 +0800

    完成弹出框按钮显示与隐藏逻辑

--- src/views/index/components/mypop.vue
+++ src/views/index/components/mypop.vue
@@ -22,12 +22,17 @@
     </template>
     <template slot="default">
         <!-- slot="default":自定义右侧标题,按钮通过自带的属性进行修改 -->
-        <van-button plain hairline round size="mini" type="danger">编辑</van-button>
+        <van-button plain hairline round v-if="showIcon===false" @click="showIcon=true" size="mini" type="danger">编辑</van-button>
+        <van-button plain hairline round v-if="showIcon===true" @click="showIcon=false" size="mini" type="danger">完成</van-button>
     </template>
 </van-cell>
 <!-- 使用Grid 宫格设置标签的弹出框的子选项布局 -->
 <van-grid>
-  <van-grid-item v-for="(item, index) in 9 " :key="index" text="文字" />
+  <van-grid-item class="myfn" v-for="(item, index) in 9 " :key="index" >
+      <template slot="text">
+          <span class="myName">文字</span><van-icon v-show="showIcon" class="mysn" name="clear" />
+      </template>
+  </van-grid-item>
 </van-grid>
 <!-- 设置频道推荐区域 -->
 <van-cell>
@@ -46,6 +51,11 @@
 <script>
 export default {
   props: ['value'],
+  data () {
+    return {
+      showIcon: false // 频道选项修改
+    }
+  },
   methods: {
     close () {
       this.$emit('input', false)
@@ -54,8 +64,20 @@ export default {
 }
 </script>
 
-<style>
+<style lang="less">
 .popicon {
     font-size: 30px;
 }
+.myfu {
+    position: relative;
+}
+.mysn {
+  position: absolute;
+  top: 0;
+  right: 0;
+}
+.myName {
+  color: #646566;
+}
+
 </style>
