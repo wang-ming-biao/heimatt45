5117e6121dff3c2659a7b82aae38ddb42ee019ed
wang-ming-biao <1340921277>
2020-02-11 08:35:24 +0800

    导入dayjs时间插件及中文化

--- src/main.js
+++ src/main.js
@@ -10,12 +10,31 @@ import 'vant/lib/index.css'
 // 导入 iconfont
 import './style/index.css'
 
-Vue.config.productionTip = false
+// 导入 day.js
+import dayjs from 'dayjs'
+
+// 导入 dayjs 的插件
+import relativeTime from 'dayjs/plugin/relativeTime'
+
+// 导入 dayjs 中文语言包
+import 'dayjs/locale/zh-cn'
+
+// 拓展这个插件
+dayjs.extend(relativeTime)
+
+// 全局使用 中文语言包
+dayjs.locale('zh-cn')
+// 使用 vant , lazyload懒加载
 Vue.use(Vant, Lazyload)
+
+// 使用dayjs,得到相对时间
+let time = dayjs().from(dayjs('1995'))
+window.console.log(time)
 
 new Vue({
   router,
   store,
   Vant,
+  dayjs,
   render: h => h(App)
 }).$mount('#app')
