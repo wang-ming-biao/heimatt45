7af0307232601b86906fe13fe9a6f0a3e56bc8a4
wang-ming-biao <1340921277>
2020-02-17 16:40:12 +0800

    完成搜索页搜索框

--- src/views/Answer/Answer.vue
+++ src/views/Answer/Answer.vue
@@ -1,12 +1,34 @@
 <template>
   <div>
-      我是问答页
+      <form action="/">
+  <van-search
+    v-model="value"
+    show-action
+    placeholder="请输入搜索关键词"
+    @search="onSearch"
+    @cancel="onCancel"
+  />
+</form>
   </div>
 </template>
 
 <script>
 export default {
-
+  data () {
+    return {
+      value: ''
+    }
+  },
+  methods: {
+    // 事件在点击键盘上的搜索/回车按钮后触发，
+    onSearch (val) {
+      this.$toast(val)
+    },
+    // 事件在点击搜索框右侧取消按钮时触发
+    onCancel () {
+      this.$toast('取消')
+    }
+  }
 }
 </script>
 
